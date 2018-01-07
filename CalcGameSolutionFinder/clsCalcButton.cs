5d79c73c46ee331b6e396d90d381e8e0f92d1492
I Gede Adi Martha Ardiana Putra <billytheburninginferno@gmail.com>
2018-01-07 18:25:38 +0800

    Initialize Project with VisualStudio Solution

---
+++ CalcGameSolutionFinder/clsCalcButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcGameSolutionFinder
+{
+    class clsCalcButton
+    {
+        /// <summary>
+        /// Name is the name of the Child Class that will be inheritance the CalcButton Base Clas
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// ProcessButton is the main process that will provide the logic of the Calculator Game Button
+        /// </summary>
+        public virtual void ProcessButton() {
+            throw new NotImplementedException("Function ProcessButton is not implemented");
+        }
+    }
+}
